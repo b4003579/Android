3453000f46d485e2efab07377aeabf469c7cce0a
b4003579 <Frank.c.ratcliffe@gmail.com>
2018-05-07 22:29:52 +0100

    App Sufficiently Operational

--- Examples/Android Studio Examples/Chat/src/com/redbear/chat/Chat.java
+++ Examples/Android Studio Examples/Chat/src/com/redbear/chat/Chat.java
@@ -1,9 +1,5 @@
 package com.redbear.chat;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
-
 import android.app.Activity;
 import android.bluetooth.BluetoothGattCharacteristic;
 import android.bluetooth.BluetoothGattService;
@@ -15,21 +11,24 @@ import android.content.IntentFilter;
 import android.content.ServiceConnection;
 import android.os.Bundle;
 import android.os.IBinder;
-import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.ToggleButton;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.UUID;
 
 public class Chat extends Activity implements View.OnClickListener {
-	private final static String TAG = Chat.class.getSimpleName();
-
-	public static final String EXTRAS_DEVICE = "EXTRAS_DEVICE";
-	private TextView tv = null;
-	private Button btnSlowWag = null;
+    public static final String EXTRAS_DEVICE = "EXTRAS_DEVICE";
+    private final static String TAG = Chat.class.getSimpleName();
+    private TextView tv = null;
+    private Button btnSlowWag = null;
     private Button btnSlowSway = null;
     private Button btnSlowLift = null;
     private Button btnMediumWag = null;
@@ -38,52 +37,64 @@ public class Chat extends Activity implements View.OnClickListener {
     private Button btnFastWag = null;
     private Button btnFastSway = null;
     private Button btnFastLift = null;
-	private String mDeviceName;
-	private String mDeviceAddress;
-	private RBLService mBluetoothLeService;
-	private Map<UUID, BluetoothGattCharacteristic> map = new HashMap<UUID, BluetoothGattCharacteristic>();
+    private ToggleButton btnSlowAuto = null;
+    private ToggleButton btnMediumAuto = null;
+    private ToggleButton btnFastAuto = null;
+    private String mDeviceName;
+    private String mDeviceAddress;
+    private RBLService mBluetoothLeService;
+    private final ServiceConnection mServiceConnection = new ServiceConnection() {
 
-	private final ServiceConnection mServiceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName componentName,
+                                       IBinder service) {
+            mBluetoothLeService = ((RBLService.LocalBinder) service)
+                    .getService();
+            if (!mBluetoothLeService.initialize()) {
+                Log.e(TAG, "Unable to initialize Bluetooth");
+                finish();
+            }
+            // Automatically connects to the device upon successful start-up
+            // initialization.
+            mBluetoothLeService.connect(mDeviceAddress);
+        }
 
-		@Override
-		public void onServiceConnected(ComponentName componentName,
-				IBinder service) {
-			mBluetoothLeService = ((RBLService.LocalBinder) service)
-					.getService();
-			if (!mBluetoothLeService.initialize()) {
-				Log.e(TAG, "Unable to initialize Bluetooth");
-				finish();
-			}
-			// Automatically connects to the device upon successful start-up
-			// initialization.
-			mBluetoothLeService.connect(mDeviceAddress);
-		}
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+            mBluetoothLeService = null;
+        }
+    };
+    private Map<UUID, BluetoothGattCharacteristic> map = new HashMap<UUID, BluetoothGattCharacteristic>();
+    private final BroadcastReceiver mGattUpdateReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            final String action = intent.getAction();
 
-		@Override
-		public void onServiceDisconnected(ComponentName componentName) {
-			mBluetoothLeService = null;
-		}
-	};
+            if (RBLService.ACTION_GATT_DISCONNECTED.equals(action)) {
+            } else if (RBLService.ACTION_GATT_SERVICES_DISCOVERED
+                    .equals(action)) {
+                getGattService(mBluetoothLeService.getSupportedGattService());
+            } else if (RBLService.ACTION_DATA_AVAILABLE.equals(action)) {
+                displayData(intent.getByteArrayExtra(RBLService.EXTRA_DATA));
+            }
+        }
+    };
 
-	private final BroadcastReceiver mGattUpdateReceiver = new BroadcastReceiver() {
-		@Override
-		public void onReceive(Context context, Intent intent) {
-			final String action = intent.getAction();
+    private static IntentFilter makeGattUpdateIntentFilter() {
+        final IntentFilter intentFilter = new IntentFilter();
 
-			if (RBLService.ACTION_GATT_DISCONNECTED.equals(action)) {
-			} else if (RBLService.ACTION_GATT_SERVICES_DISCOVERED
-					.equals(action)) {
-				getGattService(mBluetoothLeService.getSupportedGattService());
-			} else if (RBLService.ACTION_DATA_AVAILABLE.equals(action)) {
-				displayData(intent.getByteArrayExtra(RBLService.EXTRA_DATA));
-			}
-		}
-	};
+        intentFilter.addAction(RBLService.ACTION_GATT_CONNECTED);
+        intentFilter.addAction(RBLService.ACTION_GATT_DISCONNECTED);
+        intentFilter.addAction(RBLService.ACTION_GATT_SERVICES_DISCOVERED);
+        intentFilter.addAction(RBLService.ACTION_DATA_AVAILABLE);
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.second);
+        return intentFilter;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second);
 
         btnSlowWag = (Button) findViewById(R.id.btnSlowWag);
         btnSlowWag.setOnClickListener(this); // calling onClick() method
@@ -106,46 +117,149 @@ public class Chat extends Activity implements View.OnClickListener {
         btnFastLift = (Button) findViewById(R.id.btnFastLift);
         btnFastLift.setOnClickListener(this);
 
-		Intent intent = getIntent();
+        btnFastAuto = (ToggleButton) findViewById(R.id.btnFastAuto);
+        btnFastAuto.setOnClickListener(this);
+        btnMediumAuto = (ToggleButton) findViewById(R.id.btnMediumAuto);
+        btnMediumAuto.setOnClickListener(this);
+        btnSlowAuto = (ToggleButton) findViewById(R.id.btnSlowAuto);
+        btnSlowAuto.setOnClickListener(this);
 
-		mDeviceAddress = intent.getStringExtra(Device.EXTRA_DEVICE_ADDRESS);
-		mDeviceName = intent.getStringExtra(Device.EXTRA_DEVICE_NAME);
 
-		getActionBar().setTitle(mDeviceName);
-		getActionBar().setDisplayHomeAsUpEnabled(true);
+        Intent intent = getIntent();
 
-		Intent gattServiceIntent = new Intent(this, RBLService.class);
-		bindService(gattServiceIntent, mServiceConnection, BIND_AUTO_CREATE);
-	}
+        mDeviceAddress = intent.getStringExtra(Device.EXTRA_DEVICE_ADDRESS);
+        mDeviceName = intent.getStringExtra(Device.EXTRA_DEVICE_NAME);
 
-	@Override
+        getActionBar().setTitle(mDeviceName);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+
+        Intent gattServiceIntent = new Intent(this, RBLService.class);
+        bindService(gattServiceIntent, mServiceConnection, BIND_AUTO_CREATE);
+    }
+
+    //when a button is clicked communicate the user's choice over bluetooth
+    @Override
     public void onClick(View v) {
-        String str = "";
 
-        switch (v.getId()){
+        switch (v.getId()) {
 
-            case R.id.btnSlowWag: str = "slowWag";
-            break;
-            case R.id.btnSlowSway: str = "slowSway";
-            break;
-            case R.id.btnSlowLift: str = "slowLift";
-            break;
-
-            case R.id.btnMediumWag: str = "MediumWag";
+            case R.id.btnSlowWag:
+                writeToBluetooth("slowWag");
                 break;
-            case R.id.btnMediumSway: str = "MediumSway";
+            case R.id.btnSlowSway:
+                writeToBluetooth("slowSway");
                 break;
-            case R.id.btnMediumLift: str = "MediumLift";
+            case R.id.btnSlowLift:
+                writeToBluetooth("slowLift");
                 break;
 
-            case R.id.btnFastWag: str = "FastWag";
+            case R.id.btnMediumWag:
+                writeToBluetooth("MediumWag");
                 break;
-            case R.id.btnFastSway: str = "FastSway";
+            case R.id.btnMediumSway:
+                writeToBluetooth("MediumSway");
                 break;
-            case R.id.btnFastLift: str = "FastLift";
+            case R.id.btnMediumLift:
+                writeToBluetooth("MediumLift");
+                break;
+
+            case R.id.btnFastWag:
+                writeToBluetooth("FastWag");
+                break;
+            case R.id.btnFastSway:
+                writeToBluetooth("FastSway");
+                break;
+            case R.id.btnFastLift:
+                writeToBluetooth("FastLift");
+                break;
+
+            //careful with timings
+            //delays are required to stop the bluetooth communications happening too close together
+            case R.id.btnSlowAuto:
+                if (btnMediumAuto.isChecked()) {
+                    btnMediumAuto.setChecked(false);
+                    writeToBluetooth("MediumAutoOff");
+                }
+                if (btnFastAuto.isChecked()) {
+                    btnFastAuto.setChecked(false);
+                    writeToBluetooth("FastAutoOff");
+                }
+                if (btnSlowAuto.isChecked()) {
+                    new Timer().schedule(new TimerTask() {
+                        @Override
+                        public void run() {
+                            writeToBluetooth("SlowAutoOn");
+                        }
+                    }, 500);
+
+                } else {
+                    new Timer().schedule(new TimerTask() {
+                        @Override
+                        public void run() {
+                            writeToBluetooth("SlowAutoOff");
+                        }
+                    }, 500);
+                }
+                break;
+
+            case R.id.btnMediumAuto:
+                if (btnSlowAuto.isChecked()) {
+                    btnSlowAuto.setChecked(false);
+                    writeToBluetooth("SlowAutoOff");
+                }
+                if (btnFastAuto.isChecked()) {
+                    btnFastAuto.setChecked(false);
+                    writeToBluetooth("FastAutoOff");
+                }
+                if (btnMediumAuto.isChecked()) {
+                    new Timer().schedule(new TimerTask() {
+                        @Override
+                        public void run() {
+                            writeToBluetooth("MediumAutoOn");
+                        }
+                    }, 500);
+
+                } else {
+                    new Timer().schedule(new TimerTask() {
+                        @Override
+                        public void run() {
+                            writeToBluetooth("MediumAutoOff");
+                        }
+                    }, 500);
+                }
+                break;
+
+            case R.id.btnFastAuto:
+                if (btnSlowAuto.isChecked()) {
+                    btnSlowAuto.setChecked(false);
+                    writeToBluetooth("SlowAutoOff");
+                }
+                if (btnMediumAuto.isChecked()) {
+                    btnMediumAuto.setChecked(false);
+                    writeToBluetooth("MediumAutoOff");
+                }
+                if (btnFastAuto.isChecked()) {
+                    new Timer().schedule(new TimerTask() {
+                        @Override
+                        public void run() {
+                            writeToBluetooth("FastAutoOn");
+                        }
+                    }, 500);
+
+                } else {
+                    new Timer().schedule(new TimerTask() {
+                        @Override
+                        public void run() {
+                            writeToBluetooth("FastAutoOff");
+                        }
+                    }, 500);
+                }
                 break;
         }
+    }
 
+    //responsible for actually making the bluetooth communications happen
+    private void writeToBluetooth(String str) {
         BluetoothGattCharacteristic characteristic = map
                 .get(RBLService.UUID_BLE_SHIELD_TX);
         byte b = 0x00;
@@ -158,86 +272,75 @@ public class Chat extends Activity implements View.OnClickListener {
 
         characteristic.setValue(tx);
         mBluetoothLeService.writeCharacteristic(characteristic);
+
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
 
-	@Override
-	protected void onResume() {
-		super.onResume();
+        registerReceiver(mGattUpdateReceiver, makeGattUpdateIntentFilter());
+    }
 
-		registerReceiver(mGattUpdateReceiver, makeGattUpdateIntentFilter());
-	}
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            mBluetoothLeService.disconnect();
+            mBluetoothLeService.close();
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		if (item.getItemId() == android.R.id.home) {
-			mBluetoothLeService.disconnect();
-			mBluetoothLeService.close();
+            System.exit(0);
+        }
 
-			System.exit(0);
-		}
+        return super.onOptionsItemSelected(item);
+    }
 
-		return super.onOptionsItemSelected(item);
-	}
+    @Override
+    protected void onStop() {
+        super.onStop();
 
-	@Override
-	protected void onStop() {
-		super.onStop();
+        unregisterReceiver(mGattUpdateReceiver);
+    }
 
-		unregisterReceiver(mGattUpdateReceiver);
-	}
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
 
-	@Override
-	protected void onDestroy() {
-		super.onDestroy();
+        mBluetoothLeService.disconnect();
+        mBluetoothLeService.close();
 
-		mBluetoothLeService.disconnect();
-		mBluetoothLeService.close();
+        System.exit(0);
+    }
 
-		System.exit(0);
-	}
+    private void displayData(byte[] byteArray) {
+        if (byteArray != null) {
+            String data = new String(byteArray);
+            tv.append(data);
+            // find the amount we need to scroll. This works by
+            // asking the TextView's internal layout for the position
+            // of the final line and then subtracting the TextView's height
+            final int scrollAmount = tv.getLayout().getLineTop(
+                    tv.getLineCount())
+                    - tv.getHeight();
+            // if there is no need to scroll, scrollAmount will be <=0
+            if (scrollAmount > 0)
+                tv.scrollTo(0, scrollAmount);
+            else
+                tv.scrollTo(0, 0);
+        }
+    }
 
-	private void displayData(byte[] byteArray) {
-		if (byteArray != null) {
-			String data = new String(byteArray);
-			tv.append(data);
-			// find the amount we need to scroll. This works by
-			// asking the TextView's internal layout for the position
-			// of the final line and then subtracting the TextView's height
-			final int scrollAmount = tv.getLayout().getLineTop(
-					tv.getLineCount())
-					- tv.getHeight();
-			// if there is no need to scroll, scrollAmount will be <=0
-			if (scrollAmount > 0)
-				tv.scrollTo(0, scrollAmount);
-			else
-				tv.scrollTo(0, 0);
-		}
-	}
+    private void getGattService(BluetoothGattService gattService) {
+        if (gattService == null)
+            return;
 
-	private void getGattService(BluetoothGattService gattService) {
-		if (gattService == null)
-			return;
+        BluetoothGattCharacteristic characteristic = gattService
+                .getCharacteristic(RBLService.UUID_BLE_SHIELD_TX);
+        map.put(characteristic.getUuid(), characteristic);
 
-		BluetoothGattCharacteristic characteristic = gattService
-				.getCharacteristic(RBLService.UUID_BLE_SHIELD_TX);
-		map.put(characteristic.getUuid(), characteristic);
-
-		BluetoothGattCharacteristic characteristicRx = gattService
-				.getCharacteristic(RBLService.UUID_BLE_SHIELD_RX);
-		mBluetoothLeService.setCharacteristicNotification(characteristicRx,
-				true);
-		mBluetoothLeService.readCharacteristic(characteristicRx);
-	}
-
-	private static IntentFilter makeGattUpdateIntentFilter() {
-		final IntentFilter intentFilter = new IntentFilter();
-
-		intentFilter.addAction(RBLService.ACTION_GATT_CONNECTED);
-		intentFilter.addAction(RBLService.ACTION_GATT_DISCONNECTED);
-		intentFilter.addAction(RBLService.ACTION_GATT_SERVICES_DISCOVERED);
-		intentFilter.addAction(RBLService.ACTION_DATA_AVAILABLE);
-
-		return intentFilter;
-	}
+        BluetoothGattCharacteristic characteristicRx = gattService
+                .getCharacteristic(RBLService.UUID_BLE_SHIELD_RX);
+        mBluetoothLeService.setCharacteristicNotification(characteristicRx,
+                true);
+        mBluetoothLeService.readCharacteristic(characteristicRx);
+    }
 }
--- Examples/Android Studio Examples/Chat/src/com/redbear/chat/Main.java
+++ Examples/Android Studio Examples/Chat/src/com/redbear/chat/Main.java
@@ -20,11 +20,6 @@
 
 package com.redbear.chat;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Timer;
-import java.util.TimerTask;
-
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
@@ -44,154 +39,159 @@ import android.view.Window;
 import android.widget.Button;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class Main extends Activity {
-	private BluetoothAdapter mBluetoothAdapter;
-	private static final int REQUEST_ENABLE_BT = 1;
-	private static final long SCAN_PERIOD = 3000;
-	private Dialog mDialog;
-	public static List<BluetoothDevice> mDevices = new ArrayList<BluetoothDevice>();
-	public static Main instance = null;
+    private BluetoothAdapter mBluetoothAdapter;
+    private static final int REQUEST_ENABLE_BT = 1;
+    private static final long SCAN_PERIOD = 3000;
+    private Dialog mDialog;
+    public static List<BluetoothDevice> mDevices = new ArrayList<BluetoothDevice>();
+    public static Main instance = null;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
-		setContentView(R.layout.main);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
+        setContentView(R.layout.main);
 
-		if (!getPackageManager().hasSystemFeature(
-				PackageManager.FEATURE_BLUETOOTH_LE)) {
-			Toast.makeText(this, "Ble not supported", Toast.LENGTH_SHORT)
-					.show();
-			finish();
-		}
+        if (!getPackageManager().hasSystemFeature(
+                PackageManager.FEATURE_BLUETOOTH_LE)) {
+            Toast.makeText(this, "Ble not supported", Toast.LENGTH_SHORT)
+                    .show();
+            finish();
+        }
 
-		final BluetoothManager mBluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
-		mBluetoothAdapter = mBluetoothManager.getAdapter();
-		if (mBluetoothAdapter == null) {
-			Toast.makeText(this, "Ble not supported", Toast.LENGTH_SHORT)
-					.show();
-			finish();
-			return;
-		}
+        final BluetoothManager mBluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+        mBluetoothAdapter = mBluetoothManager.getAdapter();
+        if (mBluetoothAdapter == null) {
+            Toast.makeText(this, "Ble not supported", Toast.LENGTH_SHORT)
+                    .show();
+            finish();
+            return;
+        }
 
-		if (!mBluetoothAdapter.isEnabled()) {
-			Intent enableBtIntent = new Intent(
-					BluetoothAdapter.ACTION_REQUEST_ENABLE);
-			startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
-		}
-		
-		Button btn = (Button)findViewById(R.id.btn);
-		btn.setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				scanLeDevice();
+        if (!mBluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(
+                    BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
 
-				showRoundProcessDialog(Main.this, R.layout.loading_process_dialog_anim);
+        Button btn = (Button) findViewById(R.id.btn);
+        btn.setOnClickListener(new OnClickListener() {
 
-				Timer mTimer = new Timer();
-				mTimer.schedule(new TimerTask() {
+            @Override
+            public void onClick(View v) {
+                scanLeDevice();
 
-					@Override
-					public void run() {
-						Intent deviceListIntent = new Intent(getApplicationContext(),
-								Device.class);
-						startActivity(deviceListIntent);
-						mDialog.dismiss();
-					}
-				}, SCAN_PERIOD);
-			}
-		});
+                showRoundProcessDialog(Main.this, R.layout.loading_process_dialog_anim);
 
-		scanLeDevice();
+                Timer mTimer = new Timer();
+                mTimer.schedule(new TimerTask() {
 
-		showRoundProcessDialog(Main.this, R.layout.loading_process_dialog_anim);
+                    @Override
+                    public void run() {
+                        Intent deviceListIntent = new Intent(getApplicationContext(),
+                                Device.class);
+                        startActivity(deviceListIntent);
+                        mDialog.dismiss();
+                    }
+                }, SCAN_PERIOD);
+            }
+        });
 
-		Timer mTimer = new Timer();
-		mTimer.schedule(new TimerTask() {
+        scanLeDevice();
 
-			@Override
-			public void run() {
-				Intent deviceListIntent = new Intent(getApplicationContext(),
-						Device.class);
-				startActivity(deviceListIntent);
-				mDialog.dismiss();
-			}
-		}, SCAN_PERIOD);
+        showRoundProcessDialog(Main.this, R.layout.loading_process_dialog_anim);
 
-		instance = this;
-	}
+        Timer mTimer = new Timer();
+        mTimer.schedule(new TimerTask() {
 
-	public void showRoundProcessDialog(Context mContext, int layout) {
-		OnKeyListener keyListener = new OnKeyListener() {
-			@Override
-			public boolean onKey(DialogInterface dialog, int keyCode,
-					KeyEvent event) {
-				if (keyCode == KeyEvent.KEYCODE_HOME
-						|| keyCode == KeyEvent.KEYCODE_SEARCH) {
-					return true;
-				}
-				return false;
-			}
-		};
+            @Override
+            public void run() {
+                Intent deviceListIntent = new Intent(getApplicationContext(),
+                        Device.class);
+                startActivity(deviceListIntent);
+                mDialog.dismiss();
+            }
+        }, SCAN_PERIOD);
 
-		mDialog = new AlertDialog.Builder(mContext).create();
-		mDialog.setOnKeyListener(keyListener);
-		mDialog.show();
-		// 娉ㄦ��姝ゅ��瑕���惧��show涔���� ������浼���ュ��甯�
-		mDialog.setContentView(layout);
-	}
+        instance = this;
+    }
 
-	private void scanLeDevice() {
-		new Thread() {
+    public void showRoundProcessDialog(Context mContext, int layout) {
+        OnKeyListener keyListener = new OnKeyListener() {
+            @Override
+            public boolean onKey(DialogInterface dialog, int keyCode,
+                                 KeyEvent event) {
+                if (keyCode == KeyEvent.KEYCODE_HOME
+                        || keyCode == KeyEvent.KEYCODE_SEARCH) {
+                    return true;
+                }
+                return false;
+            }
+        };
 
-			@Override
-			public void run() {
-				mBluetoothAdapter.startLeScan(mLeScanCallback);
+        mDialog = new AlertDialog.Builder(mContext).create();
+        mDialog.setOnKeyListener(keyListener);
+        mDialog.show();
+        // 娉ㄦ��姝ゅ��瑕���惧��show涔���� ������浼���ュ��甯�
+        mDialog.setContentView(layout);
+    }
 
-				try {
-					Thread.sleep(SCAN_PERIOD);
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
+    private void scanLeDevice() {
+        new Thread() {
 
-				mBluetoothAdapter.stopLeScan(mLeScanCallback);
-			}
-		}.start();
-	}
+            @Override
+            public void run() {
+                mBluetoothAdapter.startLeScan(mLeScanCallback);
 
-	private BluetoothAdapter.LeScanCallback mLeScanCallback = new BluetoothAdapter.LeScanCallback() {
+                try {
+                    Thread.sleep(SCAN_PERIOD);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
 
-		@Override
-		public void onLeScan(final BluetoothDevice device, final int rssi,
-				byte[] scanRecord) {
-			runOnUiThread(new Runnable() {
-				@Override
-				public void run() {
-					if (device != null) {
-						if (mDevices.indexOf(device) == -1)
-							mDevices.add(device);
-					}
-				}
-			});
-		}
-	};
+                mBluetoothAdapter.stopLeScan(mLeScanCallback);
+            }
+        }.start();
+    }
 
-	@Override
-	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-		// User chose not to enable Bluetooth.
-		if (requestCode == REQUEST_ENABLE_BT
-				&& resultCode == Activity.RESULT_CANCELED) {
-			finish();
-			return;
-		}
-		super.onActivityResult(requestCode, resultCode, data);
-	}
+    private BluetoothAdapter.LeScanCallback mLeScanCallback = new BluetoothAdapter.LeScanCallback() {
 
-	@Override
-	protected void onDestroy() {
-		super.onDestroy();
+        @Override
+        public void onLeScan(final BluetoothDevice device, final int rssi,
+                             byte[] scanRecord) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    if (device != null) {
+                        if (mDevices.indexOf(device) == -1)
+                            mDevices.add(device);
+                    }
+                }
+            });
+        }
+    };
 
-		System.exit(0);
-	}
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // User chose not to enable Bluetooth.
+        if (requestCode == REQUEST_ENABLE_BT
+                && resultCode == Activity.RESULT_CANCELED) {
+            finish();
+            return;
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        System.exit(0);
+    }
 }
